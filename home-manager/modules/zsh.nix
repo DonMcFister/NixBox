08be0c761fa1107f4cc0fd379255155060f6e5e8
DonMcFister <kwiatkowski.dawid@pm.me>
2026-05-01 12:19:47 +0100

    Create zsh.nix

---
+++ home-manager/modules/zsh.nix
@@ -0,0 +1,30 @@
+{ config, ... }: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases =
+      let
+        flakeDir = "~/flake";
+      in {
+        sw = "nh os switch";
+        upd = "nh os switch --update";
+        hms = "nh home switch";
+
+        pkgs = "nvim ${flakeDir}/nixos/packages.nix";
+
+        gs = "git status";
+        ga = "git add";
+        gc = "git commit";
+        gp = "git push";
+
+        ".." = "cd ..";
+      };
+
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+
+  };
+}
