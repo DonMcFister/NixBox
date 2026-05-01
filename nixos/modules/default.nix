f4d3b26807efe472328274b43fd5c86ef2206faa
DonMcFister <kwiatkowski.dawid@pm.me>
2026-05-01 11:27:48 +0100

    Create default.nix

---
+++ nixos/modules/default.nix
@@ -0,0 +1,16 @@
+{
+  imports = [
+    ./audio.nix
+    ./bluetooth.nix
+    ./env.nix
+    ./home-manager.nix
+    ./hyprland.nix
+    ./kernel.nix
+    ./mime.nix
+    ./net.nix
+    ./nh.nix
+    ./nix.nix
+    ./timezone.nix
+    ./user.nix
+  ];
+}
